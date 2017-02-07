64a7dd3a3fca0dd6686e17bbaf9f45a0057392a2
tlh <tlh@MyWin8>
2017-02-07 11:41:32 -0500

    BestCommit

--- ICSGenerator/src/generate/ics/Generate.java
+++ ICSGenerator/src/generate/ics/Generate.java
@@ -52,6 +52,8 @@ public class Generate {
             bw.write(calEnd);
 
             System.out.println("STuff");
+            System.out.println("STuff");
+            System.out.println("STuff");
             bw.close();
 
 
